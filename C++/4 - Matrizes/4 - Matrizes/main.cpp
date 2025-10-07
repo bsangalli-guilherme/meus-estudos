9b963eb22ed67f965f0ce9d9dde702e7090f98be
bsangalli-guilherme <bsangalli.guilherme@gmail.com>
2025-10-07 20:02:58 -0300

    atualizzação do código, incluindo tamanho variavel da matriz, e tirei a necessidade do user digitar cada elemento, agora são gerados aleatóriamente de 0 a 9

--- C++/4 - Matrizes/4 - Matrizes/main.cpp
+++ C++/4 - Matrizes/4 - Matrizes/main.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <locale.h>
+#include <random>
+#include <vector>
+
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "");
 
-	int mat[3][3], soma = 0;
+	int n;
+	cout << "Digite o tamanho da matriz (N): ";
+	if(!(cin >> n) || n<=0) return 0;
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << "Elemento [" << i << "][" << j << "]: ";
-			cin >> mat[i][j];
+	//Cria um vetor de vetores dinamico 
+	vector<vector<int>> mat(n, vector<int>(n));
+
+	random_device rd; //seed aleatória, coleta entropia real do hardware (se disponível) -> pode usar clock, movimentos do mouse, ruido elétrico, etc...
+	mt19937 gen(rd());// mersenne twister, algoritmo que gera uma sequência deterministica de números aleatórios, baseado nesse caso na seed rd
+	uniform_int_distribution<> dist(0, 9); //define como os números são gerados 
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			mat[i][j] = dist(gen);
+			cout << mat[i][j] << " ";
 		}
+		cout << "\n";
 	}
 
-	
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (i == j) {
-				soma += mat[i][j];
-			}
+	int soma = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			soma += mat[i][i];
 		}
-		cout << endl;
+		cout << "A soma da diagonal principal é: " << soma << endl;
+
+		return 0;
 	}
-	cout << "A soma da diagonal principal é: " << soma << endl;
-
-	return 0;
-
 }
 
 
