eaac5a93e05727b69376f44d9f2e43dd379da5e0
bsangalli-guilherme <bsangalli.guilherme@gmail.com>
2025-11-12 11:46:59 -0300

    Learning C++

--- C++/1.1 - Loops e condicionais/Condicionais e laços de repetição 2/main.cpp
+++ C++/1.1 - Loops e condicionais/Condicionais e laços de repetição 2/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream> // biblioteca de entrada de dados 
 #include <locale.h> // configuração de localidade
-using namespace std;//faz com eu que não precise usar std::cout e std::cin -> uso só cin e cout
 
 int main() {
 	setlocale(LC_ALL, "");//usa local padrão do PC
 	
 	int n=0, soma = 0, i = 1; //declaração do valor
-	cout << "Digite um número inteiro positivo: ";
-	cin >> n;
+	std::cout << "Digite um número inteiro positivo: ";
+	std::cin >> n;
+
 
 
 	if (n > 0) {
@@ -15,7 +15,7 @@ int main() {
 		for (i; i <= n; i++) {
 			soma += i;
 		}
-		cout << "A soma de 1 até " << n << " é de " << soma << endl;
+		std::cout << "A soma de 1 até " << n << " é de " << soma << std::endl;
 	}
 	return 0;
 }
